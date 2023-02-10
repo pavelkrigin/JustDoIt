285bd650c968ca5d2ae73ad07bce70f73fa59ddc
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-02-10 19:55:14 +0100

    work_commit_10.02.23

--- JustDoIt/NewTaskViewController.swift
+++ JustDoIt/NewTaskViewController.swift
@@ -8,7 +8,20 @@
 import UIKit
 
 final class NewTaskViewController: UIViewController {
+ 
+    @IBOutlet var taskTextView: UITextView!
     
+    @IBOutlet var prioritySegmentedControl: UISegmentedControl!
+    
+    @IBOutlet var doneButton: UIButton!
+    
+    @IBOutlet var bottomConstraint: NSLayoutConstraint!
+    
+    @IBAction func doneButtonPresssed(_ sender: Any) {
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: Any) {
+    }
 }
 
 
