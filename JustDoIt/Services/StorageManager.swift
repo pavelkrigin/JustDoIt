790fe9fbe14367d5e0568e0d8415a3ff684481a9
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-02-14 14:23:18 +0300

    entrance_point_commit

--- JustDoIt/Services/StorageManager.swift
+++ JustDoIt/Services/StorageManager.swift
@@ -5,4 +5,18 @@
 //  Created by Pavel Krigin on 14.02.2023.
 //
 
-import Foundation
+import CoreData
+
+final class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    // MARK: - Core Data Stack
+    
+    private let persistentContainer: NSPersistentContainer = {
+        let container  = NSPersistentContainer(name: "Just Do It")
+        container.loadPersistentStores(completionHandler: <#T##(NSPersistentStoreDescription, Error?) -> Void#>)
+    }
+    
+    private init() {}
+}
