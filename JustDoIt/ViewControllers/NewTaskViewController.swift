f55768fa6f555532a24007e3e0ad10651e4320b1
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-02-14 20:13:56 +0300

    fixing_git_problem

--- JustDoIt/ViewControllers/NewTaskViewController.swift
+++ JustDoIt/ViewControllers/NewTaskViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class NewTaskViewController: UIViewController {
+final class NewTaskViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
