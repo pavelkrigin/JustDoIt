fdb6151b3068e841f224b8ce734d4eb49a3b66cd
Pavel Krigin <pavel.krigin.85@gmail.com>
2023-02-14 18:52:04 +0300

    getFetchedResultsController_commit

--- JustDoIt/Services/StorageManager.swift
+++ JustDoIt/Services/StorageManager.swift
@@ -29,12 +29,29 @@ final class StorageManager {
     
     private init() {}
     
+    func getFetchedResultsController(entityName: String, keyForSort: String) -> NSFetchedResultsController<NSFetchRequestResult> {
+        // Метод принимает два параметра: название сущности и параметр для сортировки записей. Это позволит определять по какому полю делать сортировку задач
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: entityName) // запрос для выборки данных из базы
+        let sortDescriptor = NSSortDescriptor(key: keyForSort, ascending: true) // объект NSSortDescriptor с параметрами, используемыми для сортировки данных
+        
+        fetchRequest.sortDescriptors = [sortDescriptor] // объект NSSortDescriptor добавляется в массив
+        
+        let fetchResultsController = NSFetchedResultsController( // Создание экземпляра класса NSFetchedResultsController на основе, созданного ранее запроса и параметров сортировки
+            fetchRequest: fetchRequest,
+            managedObjectContext: viewContext,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+            return fetchResultsController //функция возвращает созданный экземпляр
+        
+    }
+    
     
     // MARK: - Core Data Saving support
     func saveContext() { // реализация метода для сохранения контекста
         if viewContext.hasChanges {
             do {
-                try.viewContext.save()
+                try viewContext.save()
             } catch {
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
